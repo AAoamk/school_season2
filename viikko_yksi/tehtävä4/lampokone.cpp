28dc7d494f42af158386ab023b334cf57e47d865
aleksi <t9alal00@students.oamk.fi>
2020-08-30 22:59:38 +0300

    lämpökone

---
+++ viikko_yksi/tehtävä4/lampokone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "c -> f vai f - > c ?" "\n";
+    string tila;
+    cout << "anna lahtoarvon kirjain, c tai f" "\n";
+    getline (cin, tila);
+    cout << "valitsit " << tila << "\n";
+    if (tila == "c") {
+        int heat1;
+        cout << "lampotilasi celsiuksina?";
+        cin >> heat1;
+        int cel;
+        cel = heat1*1.8+32;
+        cout << cel;
+    }
+    else {
+        int heat2;
+        cout << "lampotilasi fahrenheitteina";
+        cin >> heat2;
+        int fah;
+        fah = heat2/1.8-32;
+        cout << fah;
+    }
+}
